ced4e0385449c82586f4ceeeb3bb156e53e13a9d
beepscore <support@beepscore.com>
2017-10-23 19:59:30 -0700

    Add Date related statements

--- applied_statistics_for_data_mining_and_machine_learning/ch01.R
+++ applied_statistics_for_data_mining_and_machine_learning/ch01.R
@@ -1,3 +1,6 @@
+# https://stackoverflow.com/questions/42638145/timezone-error-reading-csv
+#options(readr.default_locale=readr::locale(tz="UTC"))
+
 A = c(1, 2, 3)
 B = c(11, 22, 33)
 
@@ -10,8 +13,23 @@ print(f)
 c <- "Hello World"
 print(c)
 
+# assigning via <- vs =
+# https://stackoverflow.com/questions/1741820/assignment-operators-in-r-and#1742550
 n <- 10
 cat('n', n, '\n')
 0.3 -> s
 cat('s', s, '\n')
+m = TRUE
 
+x <- as.Date("2017-10-23")
+
+x <- Sys.time()
+> x
+# [1] "2017-10-24 02:55:54 GMT"
+
+p <- as.POSIXlt(x)
+names(unclass(p))
+# [1] "sec"    "min"    "hour"   "mday"   "mon"    "year"   "wday"   "yday"
+# [9] "isdst"  "zone"   "gmtoff"
+p$sec
+# [1] 54.43315
